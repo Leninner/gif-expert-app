a51392ea2fafde38a6f221c7d94f4472a2f4477c
leninner <mazabandalenin180@gmail.com>
2022-02-02 16:37:44 -0500

    Added mock for simulate fetch of data

--- src/test/components/GifGrid.test.js
+++ src/test/components/GifGrid.test.js
@@ -1,8 +1,14 @@
 import { shallow } from 'enzyme';
 import { GifGrid } from '../../components/GifGrid';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+
+jest.mock('../../hooks/useFetchGifs');
 
 describe('Vamos a tener una buena prueba', () => {
   const category = 'One Punch';
+  const gifs = [{ id: 'ABC', url: 'https://localhost/one-punch.gif', title: 'One Punch' }];
+
+  useFetchGifs.mockReturnValue({ data: gifs, loading: true });
 
   const wrapper = shallow(<GifGrid category={category} />);
 
