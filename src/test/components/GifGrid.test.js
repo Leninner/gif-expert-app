90996d14ee38344c60c0a556b3ffc4732dd58432
leninner <mazabandalenin180@gmail.com>
2022-02-06 21:55:21 -0500

    Update test

--- src/test/components/GifGrid.test.js
+++ src/test/components/GifGrid.test.js
@@ -6,15 +6,18 @@ jest.mock('../../hooks/useFetchGifs');
 
 describe('Vamos a tener una buena prueba', () => {
   const category = 'One Punch';
-  const gifs = [{ id: 'ABC', url: 'https://localhost/one-punch.gif', title: 'One Punch' }];
+  const gifs = [
+    { id: 'ABC', url: 'https://localhost/one-punch.gif', title: 'One Punch' },
+    { id: '123', url: 'https://localhost/one-punch.gif', title: 'One Punch' },
+    { id: '222', url: 'https://localhost/one-punch.gif', title: 'One Punch' },
+  ];
 
   useFetchGifs.mockReturnValue({ data: gifs, loading: true });
 
   const wrapper = shallow(<GifGrid category={category} />);
 
-  test('Debe coincidir con el snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
+  test('Debe dar buenos resultados', () => {
+    expect(wrapper.find('p').exists()).toBe(false);
+    expect(wrapper.find('GifGridItem').length).toBe(gifs.length);
   });
-
-  test('Debe mostrar las imágenes cuando se va cargando', () => {});
 });
